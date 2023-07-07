e0c595bde336b8480e20224f784b5982701ef97b
nrimsky <ninarimsky@gmail.com>
2023-07-07 16:13:42 +0100

    Initial commit

---
+++ FLAN.py
@@ -0,0 +1,152 @@
+"""
+Finetune flan on a dataset
+"""
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from tqdm import tqdm
+import argparse
+import json
+import shutil
+import os
+from datetime import datetime   
+from glob import glob
+
+max_length = 128
+
+def save_checkpoint(model, epoch, iteration, save_to, max_checkpoints=2):
+    checkpoint_path = f"{save_to}/checkpoint-{epoch}-{iteration}"
+    model.save_pretrained(checkpoint_path)
+
+    # Delete older checkpoints
+    checkpoint_dirs = [d for d in os.listdir(save_to) if d.startswith("checkpoint-")]
+    if len(checkpoint_dirs) > max_checkpoints:
+        oldest_checkpoint = min(checkpoint_dirs, key=lambda d: int(d.split("-")[2]))
+        shutil.rmtree(os.path.join(save_to, oldest_checkpoint))
+
+def make_model_name():
+    now = datetime.now()
+    return f"flan-finetuned-{now.day}-{now.hour}-{now.minute}-{now.second}"
+
+class CustomDataset(Dataset):
+    def __init__(self, data, tokenizer, max_length):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        input_text, target_text = item["input_text"], item["target_text"]
+        encoding = self.tokenizer(input_text, max_length=self.max_length, padding="max_length", truncation=True, return_tensors="pt")
+        target_encoding = self.tokenizer(target_text, max_length=self.max_length, padding="max_length", truncation=True, return_tensors="pt")
+
+        return {
+            "input_ids": encoding["input_ids"].flatten(),
+            "attention_mask": encoding["attention_mask"].flatten(),
+            "labels": target_encoding["input_ids"].flatten(),
+        }
+
+def train(model, dataloader, optimizer, device, save_to, epoch):
+    model.train()
+    total_loss = 0
+    num_batches = len(dataloader)
+    for idx, batch in enumerate(tqdm(dataloader)):
+        optimizer.zero_grad()
+        input_ids = batch["input_ids"].to(device)
+        attention_mask = batch["attention_mask"].to(device)
+        labels = batch["labels"].to(device)
+
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+
+        total_loss += loss.item()
+
+        # Save checkpoints
+        if idx == num_batches // 3 or idx == (num_batches * 2) // 3:
+            save_checkpoint(model, epoch, idx, save_to)
+
+    return total_loss / len(dataloader)
+
+def read_data(data_path):
+    with open(data_path, 'r') as dfile:
+        return json.load(dfile)
+
+def finetune(data_path, model_path="./flan-t5-small", num_epochs=5, learning_rate=0.001, batch_size=4, save_to=None):
+    """
+    Data should have format:
+    [
+        {"input_text": "Example input 1", "target_text": "Example target 1"},
+        {"input_text": "Example input 2", "target_text": "Example target 2"},
+        ...
+    ]
+    """
+    torch.cuda.empty_cache()
+    if save_to:
+        save_dir = save_to
+    else:
+        save_dir = make_model_name()
+    data = read_data(data_path)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device", device)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path, local_files_only=True)
+    model  = model.to(device)
+    dataset = CustomDataset(data, tokenizer, max_length)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    optimizer = Adam(model.parameters(), lr=learning_rate)
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        train_loss = train(model, dataloader, optimizer, device, save_dir, epoch)
+        print(f"Training loss: {train_loss:.4f}")
+    model.save_pretrained(save_dir)
+    print("Saved finetuned model to", save_dir)
+
+def generate_text(prompt, model, tokenizer, device):
+    model.eval()
+    input_ids = tokenizer(prompt, return_tensors="pt")["input_ids"].to(device)
+    output_ids = model.generate(input_ids, max_new_tokens=max_length)[0]
+    return tokenizer.decode(output_ids, skip_special_tokens=True)
+
+def make_prompt(recipe):
+    return "List the ingredients for: " + recipe
+
+def prepend_recipe_task_to_prompt(dataset):
+    for item in dataset:
+        item["input_text"] = make_prompt(item["input_text"])
+    return dataset
+
+def finetune_on_recipe_task():
+    files = glob("./datasets/cooking/*.json")
+    data = []
+    for file in files:
+        with open(file, 'r') as f:
+            data.extend(json.load(f))
+    data = prepend_recipe_task_to_prompt(data)
+    with open("./datasets/cooking.json", 'w') as f:
+        json.dump(data, f)
+    path = "./datasets/cooking.json"
+    finetune(path, save_to="flan-finetuned-cooking")
+
+def inference_loop(model_path = "./flan-finetuned-cooking", tokenizer_path = "./flan-t5-small"):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path, local_files_only=True)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    while True:
+        recipe_name = input("Enter a recipe name, or 'q' to quit: ")
+        if recipe_name == 'q':
+            break
+        print(generate_text(make_prompt(recipe_name), model, tokenizer, device))
+
+if __name__ == "__main__":
+    # finetune_on_recipe_task()
+    inference_loop()
---
+++ generate_dataset.py
@@ -0,0 +1,58 @@
+import openai
+from dotenv import load_dotenv
+import os
+import json
+from cuisines import FOOD_CATEGORIES
+import os
+from time import sleep
+
+load_dotenv()
+api_key = os.environ.get("API_KEY")
+
+openai.api_key = api_key
+
+PROMPT = """You are an assistant that generates a dataset, formatted as a JSON list. The structure of the dataset should be like:
+[
+{"input_text": "Chicken Pie", "target_text": "chicken, vegetables, flour, butter, milk, salt, pepper, bay leaf"},
+{"input_text": "Vanilla Cake", "target_text": "sugar, flour, butter, eggs, vanilla extract, baking powder, milk"},
+...
+]
+Please include at least 20 entries in the dataset. *Do not* include any additional text besides the dataset itself."""
+
+def get_dataset(dataset_description, save_to):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": PROMPT,
+            },
+            {"role": "user", "content": dataset_description},
+        ],
+    )
+    statements = response["choices"][0]["message"]["content"].strip()
+    try:
+        statements = json.loads(statements)
+        with open(save_to, "w") as sfile:
+            json.dump(statements, sfile)
+    except json.decoder.JSONDecodeError:
+        print("Error: dataset not formatted as a JSON list")
+        with open(save_to, "w") as sfile:
+            sfile.write(statements)
+
+if __name__ == "__main__":
+    dir = os.path.join("datasets", "cooking")
+    for x in FOOD_CATEGORIES:
+        filename = os.path.join(dir, f'{"_".join(x.split(" "))}_recipes.json')
+        # check if file exists
+        if os.path.exists(filename):
+            continue
+        for _ in range(3):
+            try:
+                get_dataset(f"Generate a dataset mapping {x} recipe names to comma seperated ingredients with spaces. Recipe names and ingredients should be in English (for a US audience).", filename)
+                print(f"Saved dataset to {filename}")
+                break
+            except openai.error.ServiceUnavailableError:
+                # rate limit exceeded
+                print("Rate limit exceeded, waiting 20 seconds")
+                sleep(20)
