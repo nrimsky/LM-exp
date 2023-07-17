4d095bf882cd79251fdbd43f5c3ecd6b29b57107
nrimsky <ninarimsky@gmail.com>
2023-07-17 16:32:22 -0700

    Gen data + more steering experiment

--- generate_blogger_dataset.py
+++ generate_blogger_dataset.py
@@ -9,13 +9,13 @@ api_key = os.environ.get("API_KEY")
 
 openai.api_key = api_key
 
-PROMPT = """You are an assistant that generates a dataset, formatted as a JSON list. The structure of the dataset should be like:
+PROMPT = """You are an assistant that generates blogpost introductions, formatted as a JSON list. The structure of the dataset should be like:
 [
-{"bio": "<some description of a person>"},
-{"bio": "<some description of a person>"},
+{"bio": "Hi, my name is Tom and I am starting this blog to discuss my ideas and opinions. I am philosophy professor at the University of Cambridge studying morality."},
+{"bio": "Hello, I'm Katherine and this is my personal blog about my worldview and perspectives! My day job is as a software engineer at Google. My passion is arts and crafts."},
 ...
 ]
-Please include at least 20 entries in the dataset. *Do not* include any additional text besides the dataset itself."""
+Please include at least 30 entries in the dataset. *Do not* include any additional text besides the dataset itself."""
 
 def get_dataset(dataset_description, save_to):
     response = openai.ChatCompletion.create(
@@ -39,4 +39,4 @@ def get_dataset(dataset_description, save_to):
             sfile.write(statements)
 
 if __name__ == "__main__":
-    get_dataset(f"Generate a dataset of 30 short bios of randomly sampled Americans.", "datasets/american_bios.json")
+    get_dataset(f"Generate a dataset of 30 short diverse blogpost introductions.", "datasets/blog_intros.json")
