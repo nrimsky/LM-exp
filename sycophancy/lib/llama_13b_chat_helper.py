2d2404cf6141ac2aea330b3e1f6ac22b5326f5a7
nrimsky <ninarimsky@gmail.com>
2023-07-28 17:24:06 -0700

    Llama 13b chat helper

---
+++ sycophancy/lib/llama_13b_chat_helper.py
@@ -0,0 +1,163 @@
+"""
+Wrapper class for Llama2 13B Chat model 
+Provides access to reading intermediate activations, unembedding intermediate activations, and activation steering via addition
+"""
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+class AttnWrapper(torch.nn.Module):
+    def __init__(self, attn):
+        super().__init__()
+        self.attn = attn
+        self.activations = None
+        self.add_tensor = None
+
+    def forward(self, *args, **kwargs):
+        output = self.attn(*args, **kwargs)
+        if self.add_tensor is not None:
+            output = (output[0] + self.add_tensor,) + output[1:]
+        self.activations = output[0]
+        return output
+
+    def reset(self):
+        self.activations = None
+        self.add_tensor = None
+
+
+class BlockOutputWrapper(torch.nn.Module):
+    def __init__(self, block, unembed_matrix, norm):
+        super().__init__()
+        self.block = block
+        self.unembed_matrix = unembed_matrix
+        self.norm = norm
+
+        self.block.self_attn = AttnWrapper(self.block.self_attn)
+        self.post_attention_layernorm = self.block.post_attention_layernorm
+
+        self.attn_mech_output_unembedded = None
+        self.intermediate_res_unembedded = None
+        self.mlp_output_unembedded = None
+        self.block_output_unembedded = None
+
+    def forward(self, *args, **kwargs):
+        output = self.block(*args, **kwargs)
+        self.block_output_unembedded = self.unembed_matrix(self.norm(output[0]))
+        attn_output = self.block.self_attn.activations
+        self.attn_mech_output_unembedded = self.unembed_matrix(self.norm(attn_output))
+        attn_output += args[0]
+        self.intermediate_res_unembedded = self.unembed_matrix(self.norm(attn_output))
+        mlp_output = self.block.mlp(self.post_attention_layernorm(attn_output))
+        self.mlp_output_unembedded = self.unembed_matrix(self.norm(mlp_output))
+        return output
+
+    def attn_add_tensor(self, tensor):
+        self.block.self_attn.add_tensor = tensor
+
+    def reset(self):
+        self.block.self_attn.reset()
+
+    def get_attn_activations(self):
+        return self.block.self_attn.activations
+
+
+class Llama13BChatHelper:
+    def __init__(self, token, system_prompt="You are a helpful, respectful and honest assistant."):
+        """
+        token: huggingface token for Llama-2-13b-chat-hf
+        """
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.system_prompt = system_prompt
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "meta-llama/Llama-2-13b-chat-hf", use_auth_token=token
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "meta-llama/Llama-2-13b-chat-hf", use_auth_token=token
+        ).to(self.device)
+        for i, layer in enumerate(self.model.model.layers):
+            self.model.model.layers[i] = BlockOutputWrapper(
+                layer, self.model.lm_head, self.model.model.norm
+            )
+
+    def prompt_to_tokens(self, prompt):
+        B_INST, E_INST = "[INST]", "[/INST]"
+        B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+        dialog_content = B_SYS + self.system_prompt + E_SYS + prompt.strip()
+        dialog_tokens = self.tokenizer.encode(
+            f"{B_INST} {dialog_content.strip()} {E_INST}",
+            bos=True,
+            eos=False,
+        )
+        return dialog_tokens
+
+    def generate_text(self, prompt, max_length=100):
+        tokens = self.prompt_to_tokens(prompt).to(self.device)
+        generation_tokens, _ = self.model.generate(
+            prompt_tokens=[tokens],
+            max_gen_len=max_length,
+            temperature=0,
+            top_p=False,
+            logprobs=False,
+        )
+        return self.tokenizer.decode(generation_tokens)
+
+
+    def get_logits_from_prompt(self, prompt):
+        tokens = self.prompt_to_tokens(prompt).to(self.device)
+        with torch.no_grad():
+            logits = self.model(tokens).logits
+            return logits
+
+    def get_logits(self, tokens):
+        with torch.no_grad():
+            logits = self.model(tokens.to(self.device)).logits
+            return logits
+
+    def set_add_attn_output(self, layer, add_output):
+        self.model.model.layers[layer].attn_add_tensor(add_output)
+
+    def get_attn_activations(self, layer):
+        return self.model.model.layers[layer].get_attn_activations()
+
+    def reset_all(self):
+        for layer in self.model.model.layers:
+            layer.reset()
+
+    def print_decoded_activations(self, decoded_activations, label, topk=10):
+        softmaxed = torch.nn.functional.softmax(decoded_activations[0][-1], dim=-1)
+        values, indices = torch.topk(softmaxed, topk)
+        probs_percent = [int(v * 100) for v in values.tolist()]
+        tokens = self.tokenizer.batch_decode(indices.unsqueeze(-1))
+        print(label, list(zip(tokens, probs_percent)))
+
+    def decode_all_layers(
+        self,
+        text,
+        topk=10,
+        print_attn_mech=True,
+        print_intermediate_res=True,
+        print_mlp=True,
+        print_block=True,
+    ):
+        self.get_logits(text)
+        for i, layer in enumerate(self.model.model.layers):
+            print(f"Layer {i}: Decoded intermediate outputs")
+            if print_attn_mech:
+                self.print_decoded_activations(
+                    layer.attn_mech_output_unembedded, "Attention mechanism", topk=topk
+                )
+            if print_intermediate_res:
+                self.print_decoded_activations(
+                    layer.intermediate_res_unembedded,
+                    "Intermediate residual stream",
+                    topk=topk,
+                )
+            if print_mlp:
+                self.print_decoded_activations(
+                    layer.mlp_output_unembedded, "MLP output", topk=topk
+                )
+            if print_block:
+                self.print_decoded_activations(
+                    layer.block_output_unembedded, "Block output", topk=topk
+                )
